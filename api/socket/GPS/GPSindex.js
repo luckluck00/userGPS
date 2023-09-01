d17c2cb610c847bae6b583ca6c7c5a97ba136484
unknown <A9251114@g.pccu.edu.tw>
2023-09-01 14:28:08 +0800

    fourth update

--- api/socket/GPS/GPSindex.js
+++ api/socket/GPS/GPSindex.js
@@ -1,29 +1,53 @@
 const userGPS = require('../../models/userGPS');
+const jwt = require('jsonwebtoken');
+const { users, FriendsReq,Friends } = require('../../models/postgreSQL/Friend'); 
 const moment = require('moment');
-
 class GPSSocketHander{
-    connect() {
-        
-    }
+  connect(token) {
+    //const token = data.token; // 取得 Bearer token  
+    // 将 token 解析成 payload
+    const decoded = jwt.verify(token, process.env.JWT_KEY);
+    // 取得 payload 中的 id
+    const userEmail = decoded.email;
+    return userEmail;
+  }
 
     getLocation(){
         return userGPS.find();
     }
 
-    sendLocation(data){
-        const user = new userGPS({
-            latitude: data.latitude,
-            longitude: data.longitude,
-            email:data.token,
-          });
-          user.save()
-        .then(() => {
-          console.log('Location data saved to MongoDB!');
+    async sendLocation(data){
+      try{
+        const token1 = data.token;
+        // 将 token 解析成 payload
+        const decoded = jwt.verify(token1, process.env.JWT_KEY);
+        // 取得 payload 中的 Email
+        const userEmail = decoded.email;
+        const userId = decoded.id;
+        const friendEmail = await Friends.findAll({
+          where: { userId:userId},
+         });
+         const friendEmails = friendEmail.map(friend => friend.friends);
+        console.log(userEmail);
+          const userGPS1 = new userGPS({
+              latitude: data.latitude,
+              longitude: data.longitude,
+              email:userEmail,
+            });
+          const saveUserGPS = await userGPS1.save();
 
-        })
-        .catch((err) => {
-          console.error(err);
-        });
+          const formattedUserGPS = {
+            latitude: saveUserGPS.latitude,
+            longitude: saveUserGPS.longitude,
+        };
+
+        return { data: formattedUserGPS ,friendEmail: friendEmails};
+      }
+      catch(err){
+        console.error(err);
+        throw err; // 将错误重新抛出以便在调用代码中处理
+      }
+
     }
 }
 
--- api/socket/GPS/GPSsocket.js
+++ api/socket/GPS/GPSsocket.js
@@ -5,12 +5,24 @@ const getUserGPS = (io) => (socket) => {
   console.log('Client connected with id:', socket.id);
 
   const gpsSocketHandler = new GPSSocketHander();
-  gpsSocketHandler.connect();
+  socket.on('login', (token) => {
+    const userEmail = gpsSocketHandler.connect(token);
+    console.log(userEmail); 
+    socket.userEmail = userEmail;
+    socket.join(userEmail);
+    socket.emit('loggedIn', userEmail);
+  });
 
-  socket.on('sendLocation', (data) => {
-    gpsSocketHandler.sendLocation(data)
+
+  socket.on('sendLocation', async (data) => {
+    const senderEmail = socket.userEmail;
+    const GPS = await gpsSocketHandler.sendLocation(data)
+    const recipientEmailArray = GPS.friendEmail;
       try{
-        socket.emit('locationSaved', data, { message: 'Location data saved to MongoDB!' });
+        for(const recipientEmail of recipientEmailArray){
+          socket.to(recipientEmail).emit('locationSaved', { sender: senderEmail ,data: GPS.data ,message: 'Location data saved to MongoDB!' });
+          console.log(GPS.data,recipientEmail);
+        }
       }
       catch(err) {
         console.error(err);
